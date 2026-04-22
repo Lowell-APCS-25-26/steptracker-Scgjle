a03b1ccf4b4020a4f4adaec959ac664565b7acf0
Scgjle <ianplymale@gmail.com>
2026-04-22 17:08:18 +0000

    Dog ate hoemwork

--- lib/src/main/java/org/APCSLowell/StepTracker.java
+++ lib/src/main/java/org/APCSLowell/StepTracker.java
@@ -1,8 +1,28 @@
 package org.APCSLowell;
 
 public class StepTracker {
-    public StepTracker(int activityThreshold) {}
-    void addDailySteps(int stepCount) {}
-    int activeDays() {}
-    double averageSteps() {}
+    private int day = 0;
+    private double steps = 0.0;
+    private int activeday = 0; 
+    private int minsteps = 0;
+    
+    public StepTracker(int activityThreshold) {
+        minsteps = activityThreshold;
+    }
+    void addDailySteps(int stepCount) {
+        steps += stepCount;
+        day++;
+        if (stepCount >= minsteps){
+            activeday++;
+        }
+    }
+    int activeDays() {
+        return activeday;
+    }
+    double averageSteps() {
+        if (day == 0){
+            return 0;
+        }
+        return steps/day;
+    }
 }
